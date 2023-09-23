a240e5ce1426ff9f9421a8f208dc27a2457016e6
10ataniel <10ataniel@gmail.com>
2023-09-22 23:18:56 -0300

    problem_38 : solved

---
+++ problem_38/target/debug/deps/problem_38-526f217bb7451231.d
@@ -0,0 +1,5 @@
+C:\Users\Korvo\scripts\Rust\Project_Euler_Rust\problem_38\target\debug\deps\problem_38-526f217bb7451231.rmeta: src\main.rs
+
+C:\Users\Korvo\scripts\Rust\Project_Euler_Rust\problem_38\target\debug\deps\problem_38-526f217bb7451231.d: src\main.rs
+
+src\main.rs:
