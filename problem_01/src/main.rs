08607b7fa35acdd5220d2faa4f572c523e5ad1da
10ataniel <10ataniel@gmail.com>
2023-10-02 16:15:42 -0300

    problem_11_solved

--- problem_01/src/main.rs
+++ problem_01/src/main.rs
@@ -211,7 +211,6 @@ fn problem_10() {
         }
         true
     }
-
     let mut sum: u128 = 2;
     for i in (1..=2_000_000_u64).step_by(2) {
         if is_prime(i) {
@@ -221,6 +220,94 @@ fn problem_10() {
     println!("sum = {}", sum);
 }
 
+fn problem_11() {
+    const UP: [i64; 2] = [-1, 0];
+    const DOWN: [i64; 2] = [1, 0];
+    const LEFT: [i64; 2] = [0, -1];
+    const RIGHT: [i64; 2] = [0, 1];
+    const DIAGONAl_PP: [i64; 2] = [1, 1];
+    const DIAGONAl_NN: [i64; 2] = [-1, -1];
+    const DIAGONAl_PN: [i64; 2] = [1, -1];
+    const DIAGONAl_NP: [i64; 2] = [-1, 1];
+
+    
+    const directions: [[i64; 2];8] = [UP, DOWN, LEFT, RIGHT, DIAGONAl_PP,DIAGONAl_NN,DIAGONAl_PN,DIAGONAl_NP];
+
+    let matrix_str = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+        49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+        81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+        52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+        22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+        24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+        32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+        67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+        24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+        21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+        78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+        16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+        86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+        19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+        04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+        88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+        04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+        20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+        20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+        01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
+
+    let matrix_arr: Vec<u64> = matrix_str
+        .split_whitespace()
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect();
+    let matrix_chunked: Vec<_> = matrix_arr.chunks(20).collect();
+
+    let matrix_len = matrix_chunked.len() as i64;
+    let max_row = (matrix_len - 1) as i64;
+    let max_col = (matrix_chunked[0].len() - 1) as i64;
+
+    let validation = |arr:[i64;2]| -> bool {
+        let row = arr[0];
+        let col = arr[1];
+        return (row >= 0) && (row <= max_row) && (col >= 0) && (col <= max_col);
+    };
+
+    let mut max_product = 1 as u64;
+    let mut max_locations:[[i64;2];4] = [[0,0],[0,0],[0,0],[0,0]];
+
+    for row in 0..=max_row {
+        for col in 0..=max_col {
+            for direction in directions {
+                let r_hat = direction[0];
+                let c_hat = direction[1];
+                let possible_states  = [
+                    [row,col],
+                    [row + 1*r_hat, col + 1*c_hat],
+                    [row + 2*r_hat, col + 2*c_hat],
+                    [row + 3*r_hat, col + 3*c_hat],
+                ];
+                if possible_states.into_iter().all(validation) == true{
+                    let mut aux_product = 1 as u64;
+                    for valid_state in possible_states{
+                        let row = valid_state[0] as usize;
+                        let col = valid_state[1] as usize;
+                        aux_product *= matrix_chunked[row][col]
+                    }
+                    if aux_product >= max_product{
+                        max_product = aux_product;
+                        max_locations = possible_states;
+                    }
+                }
+            }
+        }
+    }
+    println!("The biggest product among all directions is : {}",max_product);
+    println!("factor, location: [ row ][ column ]");
+    for location in max_locations{
+        let row = location[0] as usize;
+        let col = location[1] as usize;
+        println!("  {}  , location: [  {} ][    {}   ]",matrix_chunked[row][col],row,col)
+    }
+}
+
 fn main() {
-    problem_10();
+    problem_11();
 }
